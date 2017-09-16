74530eb061c85094b77de7cc2325abd10a694e6c
Waddlesworth <sirwaddlesworth06@gmail.com>
2017-09-16 11:06:39 +1000

    don't send shit on unauthed

--- src/jimmy.js
+++ src/jimmy.js
@@ -6,19 +6,23 @@ import {ValidateCharacter, AddResponseType} from './auth';
 var JIMMY_TOKENS = [];
 
 function JimmyStart(message, params) {
-    var statestring = crypto.randomBytes(20).toString('hex');
+    GetCharacters(message.author.id).then(ValidateCharacter).then(function(character) {
+        var statestring = crypto.randomBytes(20).toString('hex');
 
-    var existing_token = JIMMY_TOKENS.filter(v => v.owner == message.author.id);
-    if (existing_token.length > 0) {
-        statestring = existing_token[0].state;
-    } else {
-        JIMMY_TOKENS.push({
-            state: statestring,
-            owner: message.author.id,
-        });
-    }
-    
-    message.author.send("Check this out! :slight_smile:\nhttps://jimmy.genj.io/?key=" + statestring);
+        var existing_token = JIMMY_TOKENS.filter(v => v.owner == message.author.id);
+        if (existing_token.length > 0) {
+            statestring = existing_token[0].state;
+        } else {
+            JIMMY_TOKENS.push({
+                state: statestring,
+                owner: message.author.id,
+            });
+        }
+
+        message.author.send("Check this out! :slight_smile:\nhttps://jimmy.genj.io/?key=" + statestring);
+    }).catch(function(e) {
+        message.reply("You can't use Jimmy without authentication");
+    });
 }
 
 AddResponseType('character', function(req, params) {
